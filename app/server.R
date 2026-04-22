4a3b63a5726d7869ee2ae81aee903b5130723b8d
Tian Hao Sheng <tis227@lehigh.edu>
2026-04-22 17:13:14 -0400

    cleaning and loading data done

--- app/server.R
+++ app/server.R
@@ -12,7 +12,7 @@ load_and_clean <- function() {
   # 1. remove all missing vals in important columns
   airfare_data <- airfare_data[!is.na(airfare_data$average_fare), ]
   airfare_data <- airfare_data[!is.na(airfare_data$quarter), ]
-  airfare_data <- airfare_data[!is.na(nonstop_distance), ]
+  airfare_data <- airfare_data[!is.na(airfare_data$nonstop_distance), ]
   
   # 2. filter for 2022 - 2024 only
   airfare2022 <- airfare_data[airfare_data$year == 2022, ]
@@ -35,16 +35,31 @@ load_and_clean <- function() {
   airfare2024 <- airfare2024[airfare2024$average_fare < 2000, ]
   
   # 5. convert quarter to factor for proper ordering
+  airfare2022$quarter <- as.factor(airfare2022$quarter)
+  airfare2023$quarter <- as.factor(airfare2023$quarter)
+  airfare2024$quarter <- as.factor(airfare2024$quarter)
   
+  # 6: Create quarter labels for readability
+  create_quarter_labels <- function(data) {
+    data$quarter_label <- NA
+    data$quarter_label[data$quarter == 1] <- "Q1 (Jan-Mar)"
+    data$quarter_label[data$quarter == 2] <- "Q2 (Apr-Jun)"
+    data$quarter_label[data$quarter == 3] <- "Q3 (Jul-Sep)"
+    data$quarter_label[data$quarter == 4] <- "Q4 (Oct-Dec)"
+    return(data)
+  }
   
+  airfare2022 <- create_quarter_labels(airfare2022)
+  airfare2023 <- create_quarter_labels(airfare2023)
+  airfare2024 <- create_quarter_labels(airfare2024)
+  
+  # 7. combine all years back together
+  airfare_data <- rbind(airfare2022, airfare2023, airfare2024)
+  
+  return(airfare_data)
 }
 
-titanic <- titanic_train
-titanic <- na.omit(titanic)
-titanic$Pclass <- as.factor(titanic$Pclass)
-titanic$Survived <- as.factor(titanic$Survived)
-titanic$Sex <- as.factor(titanic$Sex)
-
+# W.I.P. to change to new data
 server <- function(input, output) {
   # Create a new data frame with all possible combinations of Pclass and Sex
   all_combinations <- expand.grid(Pclass = c("1", "2", "3"), Sex = c("female", "male"))
